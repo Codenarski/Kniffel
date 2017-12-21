cb2a5b8e693f46ab59a3b68a156305f21ade4a33
Jan Bednarski <JanBednarski96@gmail.com>
2017-12-21 13:34:50 +0100

    Clean up

---
+++ cmake-build-debug/CMakeFiles/Kniffel3.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Bednarskij/CLionProjects/Kniffel3/Die.c" "C:/Users/Bednarskij/CLionProjects/Kniffel3/cmake-build-debug/CMakeFiles/Kniffel3.dir/Die.c.obj"
+  "C:/Users/Bednarskij/CLionProjects/Kniffel3/Player.c" "C:/Users/Bednarskij/CLionProjects/Kniffel3/cmake-build-debug/CMakeFiles/Kniffel3.dir/Player.c.obj"
+  "C:/Users/Bednarskij/CLionProjects/Kniffel3/main.c" "C:/Users/Bednarskij/CLionProjects/Kniffel3/cmake-build-debug/CMakeFiles/Kniffel3.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
