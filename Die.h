c3a5722246163b8089d30bc4c846b689c96d6893
Jan Bednarski <JanBednarski96@gmail.com>
2017-12-21 13:34:13 +0100

    clean up

--- Die.h
+++ Die.h
@@ -7,13 +7,4 @@
 #ifndef KNIFFEL3_DIE_H
 #define KNIFFEL3_DIE_H
 
-typedef struct Die Die;
-typedef struct Dice Dice;
-
-Die *die_initialize(Die *die, int value, bool isSelected);
-Dice *dice_initialize(Dice *dice, Die *die , int i);
-void die_destruct(Die* die);
-Die *dice_getDie(Dice *dice, int i);
-void generate_Value(Die *die);
-
 #endif //KNIFFEL3_DIE_H
